f962ce99dd7a8e5b1a9acc440bcb6e55b13c8447
motorox <motorox.srl@gmail.com>
2013-03-13 21:40:27 +0200

    Updating file structure.
    Added the necessary libs, folders, files.

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ HTML5Starter
 ============
 
 HTML5 starter project with all the dependencies.
+
+Project stub HTML5 REquireJS BBjs AmplifyJS, etc ...
---
+++ src/demo/service.php
@@ -0,0 +1,20 @@
+<?
+$requestMethod = $_SERVER["REQUEST_METHOD"];	
+
+switch ($requestMethod) {
+	case 'POST':
+		$data = json_decode(file_get_contents('php://input'), true);
+		mysql_query("INSERT INTO your_table (your_column) VALUES ('".$data["key"]."')");
+		break;
+}
+echo '[
+        { name: "Contact 1", address: "1, a street, a town, a city, AB12 3CD", tel: "0123456789", email: "anemail@me.com", type: "family" },
+        { name: "Contact 2", address: "1, a street, a town, a city, AB12 3CD", tel: "1123456789", email: "anemail@me.com", type: "family" },
+        { name: "Contact 3", address: "1, a street, a town, a city, AB12 3CD", tel: "2123456789", email: "anemail@me.com", type: "friend" },
+        { name: "Contact 4", address: "1, a street, a town, a city, AB12 3CD", tel: "3123456789", email: "anemail@me.com", type: "colleague" },
+        { name: "Contact 5", address: "1, a street, a town, a city, AB12 3CD", tel: "4123456789", email: "anemail@me.com", type: "family" },
+        { name: "Contact 6", address: "1, a street, a town, a city, AB12 3CD", tel: "5123456789", email: "anemail@me.com", type: "colleague" },
+        { name: "Contact 7", address: "1, a street, a town, a city, AB12 3CD", tel: "6123456789", email: "anemail@me.com", type: "friend" },
+        { name: "Contact 8", address: "1, a street, a town, a city, AB12 3CD", tel: "7123456789", email: "anemail@me.com", type: "family" }
+    ]';
+?>

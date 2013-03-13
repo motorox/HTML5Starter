e497dcfa3d0f57010f3411e92753af7cc0cf05d7
motorox <motorox.srl@gmail.com>
2013-03-14 00:32:36 +0200

    made the program list view work
    changed the render and append each item.

--- src/js/views/epg/programsList.js
+++ src/js/views/epg/programsList.js
@@ -16,14 +16,29 @@ define([
 		
 		initialize: function(){
 			_.bindAll(this, 'render', 'unrender', 'swap', 'remove');
+			console.log("Model in programsList view:")
 			console.log(this.model);
+			//this.listenTo(this.model, "change", this.render);
 			// this.model.bind('change', this.render);
 			// this.model.bind('remove', this.unrender);
 		},
 		
 		render: function(){
-			$(this.el).html('<span style="color:black;">' + this.model.get('part1') + ' '+ this.model.get('part2') + '</span> &nbsp; &nbsp; <span class="swap" style="font-family:sans-serif; color:blue; cursor:pointer;">[swap]</span> <span class="delete" style="cursor:pointer; color:red; font-family:sans-serif;">[delete]</span>');
+			var self = this;
+			
+			$(this.el).html('<span style="color:black;">' + this.model.get('ChannelId') + '</span> &nbsp; &nbsp; <span class="swap" style="font-family:sans-serif; color:blue; cursor:pointer;">[swap]</span> <span class="delete" style="cursor:pointer; color:red; font-family:sans-serif;">[delete]</span>');
+
+			_(this.model.get('Program')).each(function(item) {
+				self.appendItem(item);
+			}, this);
+
 			return this;
+		},
+		appendItem : function(item) {
+			console.log(item);
+			//var itemView = new ItemView({model : item});
+			//$('ul', this.el).append(itemView.render().el);
+			$(this.el).append("<div><div>" + item.Title + "</div><div>" + item.Duration + "</div><div>" + item.ShortDescription + "</div></div>");
 		},
 		
 		unrender: function(){
@@ -42,5 +57,5 @@ define([
 			this.model.destroy();
 		}
 	});
-  return new ItemView;
+  return ItemView;
 });
